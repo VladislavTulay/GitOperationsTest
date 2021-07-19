8dcb87a76f1dc295d560184594ad2d36e309201b
Vladislav Tulay <vladislav.tulai@gmail.com>
2021-07-19 20:35:55 +0300

    Added String calculator

---
+++ src/main/java/pl/calculations/StringCalculator.java
@@ -0,0 +1,34 @@
+package pl.calculations;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+class StringCalculator {
+
+    int add(String string) {
+        if (string.isEmpty()) {
+            return 0;
+        }
+        Separator separator = new Separator();
+        List<String> numbersList = separator.getNumbers(string);
+        checkForNegativeNumbers(numbersList);
+        return sumArray(numbersList);
+    }
+
+    private void checkForNegativeNumbers(List<String> numbersList) {
+        String negatives = numbersList.stream()
+                .filter(s -> s.contains("-"))
+                .collect(joining(","));
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException("Negatives not allowed: " + negatives);
+        }
+    }
+
+    private int sumArray(List<String> numbersList) {
+        return numbersList.stream()
+                .filter(s -> Integer.parseInt(s) <= 1000)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+}
