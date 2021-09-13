15052e93edddb15a6fca52dd697ab4f04e59683a
Vladislav Tulay <vladislav.tulay@gmail.com>
2021-09-13 16:13:22 +0300

    Added junit5&tags

--- src/test/java/pl/calculations/StringCalculatorTest.java
+++ src/test/java/pl/calculations/StringCalculatorTest.java
@@ -1,8 +1,10 @@
 package pl.calculations;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -11,7 +13,7 @@ public class StringCalculatorTest {
 
     private StringCalculator stringCalculator;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         stringCalculator = new StringCalculator();
     }
@@ -21,75 +23,95 @@ public class StringCalculatorTest {
         assertThat(stringCalculator.add("")).isEqualTo(0);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test()
+    @Tag("Smoke")
     public void should_throw_IllegalArgumentException_when_passed_string_is_not_a_number() {
-        stringCalculator.add("1,s,3");
+        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(",1,s,3"));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test()
+    @Tag("Smoke")
     public void should_throw_IllegalArgumentException_when_passed_string_has_comma_as_a_first_character() {
-        stringCalculator.add(",1,2,3");
+        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(",1,2,3"));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+
+    @Test()
+    @Tag("Smoke")
     public void should_throw_IllegalArgumentException_when_passed_string_has_duplicated_commas() {
-        stringCalculator.add("1,,3");
+        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("1,,3"));
     }
 
     @Test
+    @Tag("Integration")
     public void should_return_sum_when_passed_string_has_comma_as_last_character() {
         assertThat(stringCalculator.add("1,2,3,")).isEqualTo(1 + 2 + 3);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test()
+    @Tag("Integration")
     public void should_throw_IllegalArgumentException_when_passed_string_has_space_character() {
-        stringCalculator.add("1,2, 3");
+        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("1,2, 3"));
     }
 
     @Test
+    @Tag("Integration")
     public void should_throw_IllegalArgumentException_when_passed_string_has_tabulation_character() {
         Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> stringCalculator.add("1,2   ,3"));
     }
 
     @Test
+    @Tag("Func")
     public void should_return_the_same_number_when_it_was_only_one_number_passed() {
         assertThat(stringCalculator.add("3")).isEqualTo(3);
     }
 
+
     @Test
+    @Tag("Func")
     public void should_return_sum_of_every_amount_of_numbers_when_delimiter_is_only_comma() {
         assertThat(stringCalculator.add("3,2")).isEqualTo(3 + 2);
         assertThat(stringCalculator.add("3,2,6,10")).isEqualTo(3 + 2 + 6 + 10);
         assertThat(stringCalculator.add("3,2,6,10,100")).isEqualTo(3 + 2 + 6 + 10 + 100);
     }
 
+
     @Test
+    @Tag("Func")
     public void should_return_sum_of_every_amount_of_numbers_when_delimiter_is_only_new_line() {
         assertThat(stringCalculator.add("3\n2")).isEqualTo(3 + 2);
         assertThat(stringCalculator.add("3\n2\n6\n10")).isEqualTo(3 + 2 + 6 + 10);
         assertThat(stringCalculator.add("3\n2\n6\n10\n100")).isEqualTo(3 + 2 + 6 + 10 + 100);
     }
 
+
     @Test
+    @Tag("Positive")
     public void should_return_sum_of_every_amount_of_numbers_when_delimiters_are_new_lines_and_commas() {
         assertThat(stringCalculator.add("3\n2,6\n10")).isEqualTo(3 + 2 + 6 + 10);
         assertThat(stringCalculator.add("3\n2,6\n10,100")).isEqualTo(3 + 2 + 6 + 10 + 100);
     }
 
+
     @Test
+    @Tag("Positive")
     public void should_return_sum_when_single_custom_delimiter_is_specified() {
         assertThat(stringCalculator.add("//[;]\n3;2;4")).isEqualTo(3 + 2 + 4);
         assertThat(stringCalculator.add("//[*]\n3*2*4")).isEqualTo(3 + 2 + 4);
     }
 
+
     @Test
+    @Tag("Positive")
     public void should_return_sum_when_single_type_and_any_length_custom_delimiter_is_specified() {
         assertThat(stringCalculator.add("//[ddd]\n3ddd2ddd4")).isEqualTo(3 + 2 + 4);
         assertThat(stringCalculator.add("//[***]\n3***2***4")).isEqualTo(3 + 2 + 4);
     }
 
+
     @Test
+    @Tag("Positive")
     public void should_return_sum_when_multiple_type_and_any_length_custom_delimiters_are_specified() {
         assertThat(stringCalculator.add("//[ddd][fff]\n3ddd2fff4")).isEqualTo(3 + 2 + 4);
         assertThat(stringCalculator.add("//[***][%]\n3***2%4")).isEqualTo(3 + 2 + 4);
@@ -97,23 +119,29 @@ public class StringCalculatorTest {
     }
 
 
-    @Test(expected = NumberFormatException.class)
+
+    @Test()
+    @Tag("Negative")
     public void should_throw_NumberFormatException_when_delimiter_specification_is_separated_by_every_char_but_not_new_line() {
-        stringCalculator.add("//;\t3;2;4");
+        org.junit.jupiter.api.Assertions.assertThrows(NumberFormatException.class, () -> stringCalculator.add("//;\t3;2;4"));
     }
 
-    @Test(expected = NumberFormatException.class)
+
+    @Test()
+    @Tag("Negative")
     public void should_throw_NumberFormatException_when_delimiter_specification_is_not_separated_by_new_line() {
-        stringCalculator.add("//;3;2;4");
+        org.junit.jupiter.api.Assertions.assertThrows(NumberFormatException.class, () -> stringCalculator.add("//;3;2;4"));
     }
 
     @Test
+    @Tag("Negative")
     public void should_throw_IllegalArgumentException_when_negative_numbers_are_passed() {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> stringCalculator.add("-2,3,-4"))
                 .withMessage("Negatives not allowed: -2,-4");
     }
 
     @Test
+    @Tag("Negative")
     public void should_ignore_numbers_bigger_than_1000() {
         assertThat(stringCalculator.add("2,1001")).isEqualTo(2);
     }
